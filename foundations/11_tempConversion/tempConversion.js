59d7275d7f1447294c3751e625a8f096c0285c16
Chris <cpangburn@gmail.com>
2026-06-20 20:45:04 +0100

    First draft completed for tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celcius = 5/9 * (temp - 32);
+  return Number.parseFloat(celcius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let farenheit = (9/5 * temp) + 32;
+  return Number.parseFloat(farehheit.toFixed(1));
 };
 
 // Do not edit below this line
