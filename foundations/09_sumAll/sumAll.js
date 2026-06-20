35c9e6ca13176f9dd8ee720bce5d5e0983bab893
Chris <cpangburn@gmail.com>
2026-06-20 20:16:23 +0100

    Got sumAll working

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let lowest, highest;
+    if(a < b){
+        lowest = a;
+        highest = b;
+    }else{
+        lowest = b;
+        highest = a;
+    }
+    let total = 0;
+    let counter = lowest;
+    while(counter <= highest){
+        total += counter;
+        counter++;
+    }
+    return total;
 };
 
 // Do not edit below this line
