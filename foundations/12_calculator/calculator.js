d0e82c6111ebda4aa1d645f7af0a16bbd4e28ae0
Chris Pangburn <cpangburn@gmail.com>
2026-07-02 23:19:01 +0100

    Calculator complete

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.length > 0 ? nums.reduce((accum, curr) => accum + curr): 0;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((accum, curr) => accum * curr);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = num;
+  for(let i = num - 1; i > 0; i--){
+    total *= i;
+    //console.log("Total:" + total + " I:" + i);
+  }
+  total === 0 ? total = 1 : total = total;
+  return total;
 };
 
 // Do not edit below this line
